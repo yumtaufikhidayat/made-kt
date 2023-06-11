3452e7d151c6d117ada43970c3e3cb6e09ec894b
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-11 09:40:48 +0700

    Rename activity's name.

---
+++ app/src/main/java/com/yumtaufikhidayat/menjadiandroiddeveloperexpert/cleanarchitecture/CleanArchitectureMainActivity.kt
@@ -0,0 +1,12 @@
+package com.yumtaufikhidayat.menjadiandroiddeveloperexpert.cleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yumtaufikhidayat.menjadiandroiddeveloperexpert.R
+
+class CleanArchitectureMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_clean_architecture)
+    }
+}
