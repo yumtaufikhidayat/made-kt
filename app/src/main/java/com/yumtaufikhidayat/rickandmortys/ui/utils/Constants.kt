d91522977c28cba0c376731f0f111305029042be
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-10 20:22:16 +0700

    Add default time constant and adjust some constants.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/utils/Constants.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/utils/Constants.kt
@@ -1,8 +1,14 @@
-package com.yumtaufikhidayat.rickandmortys.core.utils
+package com.yumtaufikhidayat.rickandmortys.ui.utils
 
 object Constants {
+    // Api
     const val BASE_URL = "https://rickandmortyapi.com/api/"
     const val GET_CHARACTER = "character"
+
+    // DB
     const val DB_NAME = "RickMorty.db"
     const val DB_VERSION = 1
+
+    // Time
+    const val DEFAULT_TIME = "yyyy-MM-dd'T'HH:mm:ss.SS'Z'"
 }
