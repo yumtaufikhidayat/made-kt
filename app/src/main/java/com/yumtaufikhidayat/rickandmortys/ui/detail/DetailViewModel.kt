d127e4df09f15b25f4180a715066d20e7fc0e845
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-11 06:27:33 +0700

    Implement favorite in DetailFragment class.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/detail/DetailViewModel.kt
@@ -1,4 +1,16 @@
 package com.yumtaufikhidayat.rickandmortys.ui.detail
 
-class DetailViewModel {
+import androidx.lifecycle.ViewModel
+import com.yumtaufikhidayat.rickandmortys.core.domain.model.Character
+import com.yumtaufikhidayat.rickandmortys.core.domain.usecase.CharacterUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val characterUseCase: CharacterUseCase
+) : ViewModel() {
+    fun setFavoriteCharacter(character: Character, isFavorite: Boolean) {
+        characterUseCase.setFavoriteCharacter(character, isFavorite)
+    }
 }
