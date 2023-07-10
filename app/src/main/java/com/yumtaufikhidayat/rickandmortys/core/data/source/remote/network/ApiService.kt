ebe7624a1ccbab20678996f87ab981fd78238093
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-10 20:19:51 +0700

    Adjust package placement of ApiService.kt class.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/core/data/source/remote/network/ApiService.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/core/data/source/remote/network/ApiService.kt
@@ -1,7 +1,7 @@
 package com.yumtaufikhidayat.rickandmortys.core.data.source.remote.network
 
 import com.yumtaufikhidayat.rickandmortys.core.data.source.remote.response.CharacterResponse
-import com.yumtaufikhidayat.rickandmortys.core.utils.Constants
+import com.yumtaufikhidayat.rickandmortys.ui.utils.Constants
 import retrofit2.http.GET
 
 interface ApiService {
