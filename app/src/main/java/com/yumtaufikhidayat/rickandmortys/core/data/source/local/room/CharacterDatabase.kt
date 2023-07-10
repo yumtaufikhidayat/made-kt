b121787c1f70fb8c6fdc00920f10394fea661275
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-10 20:20:13 +0700

    Adjust package placement of CharacterDatabase.kt class.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/core/data/source/local/room/CharacterDatabase.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/core/data/source/local/room/CharacterDatabase.kt
@@ -3,7 +3,7 @@ package com.yumtaufikhidayat.rickandmortys.core.data.source.local.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.yumtaufikhidayat.rickandmortys.core.data.source.local.entity.CharacterEntity
-import com.yumtaufikhidayat.rickandmortys.core.utils.Constants
+import com.yumtaufikhidayat.rickandmortys.ui.utils.Constants
 
 @Database(
     entities = [CharacterEntity::class],
