2b7bc378d17519b4c6dc88cec844127a3b1dcfe2
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-10 20:19:16 +0700

    Implement details.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/detail/DetailFragment.kt
@@ -5,7 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.yumtaufikhidayat.rickandmortys.R
+import com.yumtaufikhidayat.rickandmortys.core.domain.model.Character
 import com.yumtaufikhidayat.rickandmortys.databinding.FragmentDetailBinding
+import com.yumtaufikhidayat.rickandmortys.ui.utils.Common
+import com.yumtaufikhidayat.rickandmortys.ui.utils.Common.loadImage
+import com.yumtaufikhidayat.rickandmortys.ui.utils.Common.textStatusColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +19,8 @@ class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+
+    private var character: Character? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,10 +32,43 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        handleToolbar()
+        getBundleData()
+        setDetailCharacterInformation()
+    }
+
+    private fun handleToolbar() {
+        binding.toolbarDetail.imgBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun getBundleData() {
+        character = arguments?.getParcelable(EXTRA_DATA)
+    }
+
+    private fun setDetailCharacterInformation() {
+        binding.apply {
+            val status = character?.status ?: getString(R.string.tvNA)
+            imgCharacter.loadImage(character?.image.orEmpty())
+            tvCharacterName.text = character?.name ?: getString(R.string.tvNA)
+            tvCharacterSpecies.text = character?.species ?: getString(R.string.tvNA)
+            tvCharacterGender.text = character?.gender ?: getString(R.string.tvNA)
+            tvCharacterStatus.apply {
+                text = status
+                textStatusColor(status)
+            }
+            tvCharacterCreatedAt.text = Common.covertDateToTime(character?.created.orEmpty())
+        }
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    companion object {
+        const val EXTRA_DATA = "com.yumtaufikhidayat.rickandmortys.ui.detail.EXTRA_DATA"
     }
 }
