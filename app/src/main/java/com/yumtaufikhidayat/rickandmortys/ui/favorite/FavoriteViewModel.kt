39f0adc862df37ef13539647179a03021d7088d1
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-11 09:06:27 +0700

    Implement favorite viewModel.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/favorite/FavoriteViewModel.kt
@@ -1,4 +1,14 @@
 package com.yumtaufikhidayat.rickandmortys.ui.favorite
 
-class FavoriteViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.yumtaufikhidayat.rickandmortys.core.domain.usecase.CharacterUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    characterUseCase: CharacterUseCase
+) : ViewModel() {
+    val favoriteCharacter = characterUseCase.getFavoriteCharacter().asLiveData()
 }
