e2535938e0bcb35d85217663b2b009424896f5eb
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-15 10:11:51 +0700

    Move necessary constant variable to core module.

--- app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/utils/Constants.kt
+++ app/src/main/java/com/yumtaufikhidayat/rickandmortys/ui/utils/Constants.kt
@@ -1,14 +1,6 @@
 package com.yumtaufikhidayat.rickandmortys.ui.utils
 
 object Constants {
-    // Api
-    const val BASE_URL = "https://rickandmortyapi.com/api/"
-    const val GET_CHARACTER = "character"
-
-    // DB
-    const val DB_NAME = "RickMorty.db"
-    const val DB_VERSION = 1
-
     // Time
     const val DEFAULT_TIME = "yyyy-MM-dd'T'HH:mm:ss.SS'Z'"
 }
