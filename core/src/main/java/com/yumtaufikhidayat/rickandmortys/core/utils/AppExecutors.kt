5dbff7a0748ec3045abbe359b7f8f7ddebfe4063
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-20 08:27:56 +0700

    Remove unused functions.

--- core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/AppExecutors.kt
+++ core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/AppExecutors.kt
@@ -26,10 +26,6 @@ class AppExecutors @VisibleForTesting constructor(
 
     fun diskIO(): Executor = diskIO
 
-    fun networkIO(): Executor = networkIO
-
-    fun mainThread(): Executor = mainThread
-
     private class MainThreadExecutor : Executor {
         private val mainThreadHandler = Handler(Looper.getMainLooper())
 
