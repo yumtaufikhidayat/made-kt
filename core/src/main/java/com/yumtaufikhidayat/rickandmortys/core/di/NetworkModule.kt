96ed2572d5268afe0e3dbb792e57edced0775caa
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-08 20:09:15 +0700

    Add ssl pinning.

--- core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/di/NetworkModule.kt
+++ core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/di/NetworkModule.kt
@@ -7,6 +7,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -19,11 +20,20 @@ object NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
-        val loggingInterceptor = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+        val loggingInterceptor = if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY
+        else
+            HttpLoggingInterceptor.Level.NONE
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add(Constants.WEB_URL, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(loggingInterceptor))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
--- core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/Constants.kt
+++ core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/Constants.kt
@@ -5,8 +5,11 @@ object Constants {
     const val BASE_URL = "https://rickandmortyapi.com/api/"
     const val GET_CHARACTER = "character"
 
+    // Web URL
+    const val WEB_URL = "rickandmortyapi.com"
+
     // DB
-    const val DB_NAME = "RickAndMortys.db"
+    const val DB_NAME = "RickMortys.db"
     const val DB_VERSION = 1
     const val ENCRYPTED_DB_PASSPHRASE = "rickandmortys"
 }
