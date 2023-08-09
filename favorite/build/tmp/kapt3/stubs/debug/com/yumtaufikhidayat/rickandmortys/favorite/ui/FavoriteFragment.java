602a8d444dc9a325f1ed9a7aa7c50952baaf2673
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-08-09 09:21:24 +0700

    Update project.

--- favorite/build/tmp/kapt3/stubs/debug/com/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteFragment.java
+++ favorite/build/tmp/kapt3/stubs/debug/com/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteFragment.java
@@ -1,14 +1,14 @@
 package com.yumtaufikhidayat.rickandmortys.favorite.ui;
 
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\\\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u000e\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0016\u001a\u00020\u00172\u0006\u0010\u0018\u001a\u00020\u0019H\u0016J$\u0010\u001a\u001a\u00020\u001b2\u0006\u0010\u001c\u001a\u00020\u001d2\b\u0010\u001e\u001a\u0004\u0018\u00010\u001f2\b\u0010 \u001a\u0004\u0018\u00010!H\u0016J\b\u0010\"\u001a\u00020\u0017H\u0016J\u001a\u0010#\u001a\u00020\u00172\u0006\u0010$\u001a\u00020\u001b2\b\u0010 \u001a\u0004\u0018\u00010!H\u0016J\b\u0010%\u001a\u00020\u0017H\u0002J\b\u0010&\u001a\u00020\u0017H\u0002J\u0018\u0010\'\u001a\u00020\u00172\u0006\u0010(\u001a\u00020)2\u0006\u0010*\u001a\u00020+H\u0002R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0005\u001a\u00020\u00048BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u0006\u0010\u0007R\u001e\u0010\b\u001a\u00020\t8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u000b\"\u0004\b\f\u0010\rR\u0010\u0010\u000e\u001a\u0004\u0018\u00010\u000fX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0010\u001a\u00020\u00118BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0014\u0010\u0015\u001a\u0004\b\u0012\u0010\u0013\u00a8\u0006,"}, d2 = {"Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteFragment;", "Landroidx/fragment/app/Fragment;", "()V", "_binding", "Lcom/yumtaufikhidayat/rickandmortys/favorite/databinding/FragmentFavoriteBinding;", "binding", "getBinding", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/databinding/FragmentFavoriteBinding;", "factory", "Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;", "getFactory", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;", "setFactory", "(Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;)V", "homeAdapter", "Lcom/yumtaufikhidayat/rickandmortys/ui/home/HomeAdapter;", "viewModel", "Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel;", "getViewModel", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "onAttach", "", "context", "Landroid/content/Context;", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onDestroyView", "onViewCreated", "view", "setFavoriteAdapter", "setFavoriteData", "showError", "isVisible", "", "message", "", "favorite_debug"})
+@kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000R\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\u000e\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0017\u001a\u00020\u00182\u0006\u0010\u0019\u001a\u00020\u001aH\u0016J\b\u0010\u001b\u001a\u00020\u0018H\u0016J\u001a\u0010\u001c\u001a\u00020\u00182\u0006\u0010\u001d\u001a\u00020\u001e2\b\u0010\u001f\u001a\u0004\u0018\u00010 H\u0016J\b\u0010!\u001a\u00020\u0018H\u0002J\b\u0010\"\u001a\u00020\u0018H\u0002J\u0018\u0010#\u001a\u00020\u00182\u0006\u0010$\u001a\u00020%2\u0006\u0010&\u001a\u00020\'H\u0002R\u001b\u0010\u0003\u001a\u00020\u00048BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u001e\u0010\t\u001a\u00020\n8\u0006@\u0006X\u0087.\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\f\"\u0004\b\r\u0010\u000eR\u0010\u0010\u000f\u001a\u0004\u0018\u00010\u0010X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0011\u001a\u00020\u00128BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0015\u0010\u0016\u001a\u0004\b\u0013\u0010\u0014\u00a8\u0006("}, d2 = {"Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteFragment;", "Landroidx/fragment/app/Fragment;", "()V", "binding", "Lcom/yumtaufikhidayat/rickandmortys/favorite/databinding/FragmentFavoriteBinding;", "getBinding", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/databinding/FragmentFavoriteBinding;", "binding$delegate", "Lby/kirich1409/viewbindingdelegate/ViewBindingProperty;", "factory", "Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;", "getFactory", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;", "setFactory", "(Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/factory/ViewModelFactory;)V", "homeAdapter", "Lcom/yumtaufikhidayat/rickandmortys/ui/home/HomeAdapter;", "viewModel", "Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel;", "getViewModel", "()Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "onAttach", "", "context", "Landroid/content/Context;", "onDestroyView", "onViewCreated", "view", "Landroid/view/View;", "savedInstanceState", "Landroid/os/Bundle;", "setFavoriteAdapter", "setFavoriteData", "showError", "isVisible", "", "message", "", "favorite_debug"})
 public final class FavoriteFragment extends androidx.fragment.app.Fragment {
-    @org.jetbrains.annotations.Nullable
-    private com.yumtaufikhidayat.rickandmortys.favorite.databinding.FragmentFavoriteBinding _binding;
-    @javax.inject.Inject
+    @org.jetbrains.annotations.NotNull()
+    private final by.kirich1409.viewbindingdelegate.ViewBindingProperty binding$delegate = null;
+    @javax.inject.Inject()
     public com.yumtaufikhidayat.rickandmortys.favorite.ui.factory.ViewModelFactory factory;
-    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.NotNull()
     private final kotlin.Lazy viewModel$delegate = null;
-    @org.jetbrains.annotations.Nullable
+    @org.jetbrains.annotations.Nullable()
     private com.yumtaufikhidayat.rickandmortys.ui.home.HomeAdapter homeAdapter;
     
     public FavoriteFragment() {
@@ -19,12 +19,12 @@ public final class FavoriteFragment extends androidx.fragment.app.Fragment {
         return null;
     }
     
-    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.NotNull()
     public final com.yumtaufikhidayat.rickandmortys.favorite.ui.factory.ViewModelFactory getFactory() {
         return null;
     }
     
-    public final void setFactory(@org.jetbrains.annotations.NotNull
+    public final void setFactory(@org.jetbrains.annotations.NotNull()
     com.yumtaufikhidayat.rickandmortys.favorite.ui.factory.ViewModelFactory p0) {
     }
     
@@ -32,23 +32,14 @@ public final class FavoriteFragment extends androidx.fragment.app.Fragment {
         return null;
     }
     
-    @java.lang.Override
-    public void onAttach(@org.jetbrains.annotations.NotNull
+    @java.lang.Override()
+    public void onAttach(@org.jetbrains.annotations.NotNull()
     android.content.Context context) {
     }
     
-    @java.lang.Override
-    @org.jetbrains.annotations.NotNull
-    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull
-    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable
-    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable
-    android.os.Bundle savedInstanceState) {
-        return null;
-    }
-    
-    @java.lang.Override
-    public void onViewCreated(@org.jetbrains.annotations.NotNull
-    android.view.View view, @org.jetbrains.annotations.Nullable
+    @java.lang.Override()
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
     android.os.Bundle savedInstanceState) {
     }
     
@@ -61,7 +52,7 @@ public final class FavoriteFragment extends androidx.fragment.app.Fragment {
     private final void showError(boolean isVisible, java.lang.String message) {
     }
     
-    @java.lang.Override
+    @java.lang.Override()
     public void onDestroyView() {
     }
 }
--- favorite/build/tmp/kapt3/stubs/debug/com/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel.java
+++ favorite/build/tmp/kapt3/stubs/debug/com/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel.java
@@ -1,18 +1,18 @@
 package com.yumtaufikhidayat.rickandmortys.favorite.ui;
 
 @kotlin.Metadata(mv = {1, 8, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\b\u0007\u0018\u00002\u00020\u0001B\u000f\b\u0007\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004R\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u000b"}, d2 = {"Lcom/yumtaufikhidayat/rickandmortys/favorite/ui/FavoriteViewModel;", "Landroidx/lifecycle/ViewModel;", "characterUseCase", "Lcom/yumtaufikhidayat/rickandmortys/core/domain/usecase/CharacterUseCase;", "(Lcom/yumtaufikhidayat/rickandmortys/core/domain/usecase/CharacterUseCase;)V", "favoriteCharacter", "Landroidx/lifecycle/LiveData;", "", "Lcom/yumtaufikhidayat/rickandmortys/core/domain/model/Character;", "getFavoriteCharacter", "()Landroidx/lifecycle/LiveData;", "favorite_debug"})
-@dagger.hilt.android.lifecycle.HiltViewModel
+@dagger.hilt.android.lifecycle.HiltViewModel()
 public final class FavoriteViewModel extends androidx.lifecycle.ViewModel {
-    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.NotNull()
     private final androidx.lifecycle.LiveData<java.util.List<com.yumtaufikhidayat.rickandmortys.core.domain.model.Character>> favoriteCharacter = null;
     
-    @javax.inject.Inject
-    public FavoriteViewModel(@org.jetbrains.annotations.NotNull
+    @javax.inject.Inject()
+    public FavoriteViewModel(@org.jetbrains.annotations.NotNull()
     com.yumtaufikhidayat.rickandmortys.core.domain.usecase.CharacterUseCase characterUseCase) {
         super();
     }
     
-    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.NotNull()
     public final androidx.lifecycle.LiveData<java.util.List<com.yumtaufikhidayat.rickandmortys.core.domain.model.Character>> getFavoriteCharacter() {
         return null;
     }
