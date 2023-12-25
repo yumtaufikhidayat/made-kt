7cc7f46af629acedf8bff3254930662eda448da5
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-12-25 09:25:14 +0700

    Remove unused variable in constructor and the following codes.

--- core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/AppExecutors.kt
+++ core/src/main/java/com/yumtaufikhidayat/rickandmortys/core/utils/AppExecutors.kt
@@ -1,36 +1,18 @@
 package com.yumtaufikhidayat.rickandmortys.core.utils
 
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
+    private val diskIO: Executor
 ) {
-
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
 
     @Inject
     constructor() : this(
-        Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
+        Executors.newSingleThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
-
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
 }
